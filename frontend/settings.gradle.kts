c3f20395702733f9fed21b42d7eed63b5f5f40fb
maddy13904 <67920211+maddy13904@users.noreply.github.com>
2025-12-27 15:52:52 +0530

    Add files via upload

---
+++ frontend/settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // ADDED: This repository is required for the MPAndroidChart library
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "fitnesschallenge"
+include(":app")
